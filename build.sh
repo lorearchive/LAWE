ab92f0231afd018f431620460a791ac9866f32c7
Cieron <cirrow@proton.me>
2026-03-16 19:30:55 +1300

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Install wasm-pack binary if it's not already in the path
+if ! command -v cargo &> /dev/null
+then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
 if ! command -v wasm-pack &> /dev/null
 then
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 fi
 
-pnpm run build
+pnpm build
